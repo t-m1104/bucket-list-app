0a3c60be953f6396b47465ba77de5fdd36a455fe
t-m1104 <t.marin.11j@gmail.com>
2026-09-09 23:57:23 +0900

    add frontend and CRUD features (achieve, delete)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 # ---- DBの準備 ----
 DATABASE_URL = "sqlite:///./goals.db"
@@ -20,6 +22,13 @@ Base.metadata.create_all(bind=engine)
 
 # ---- FastAPI本体 ----
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/app")
+def serve_frontend():
+    return FileResponse("static/index.html")
 
 @app.get("/")
 def read_root():
@@ -41,3 +50,23 @@ def get_goals():
     goals = db.query(Goal).all()
     db.close()
     return goals
+
+@app.patch("/goals/{goal_id}/achieve")
+def achieve_goal(goal_id: int):
+    db = SessionLocal()
+    goal = db.query(Goal).filter(Goal.id == goal_id).first()
+    if goal:
+        goal.achieved = True
+        db.commit()
+    db.close()
+    return {"message": "達成しました!"}
+
+@app.delete("/goals/{goal_id}")
+def delete_goal(goal_id: int):
+    db = SessionLocal()
+    goal = db.query(Goal).filter(Goal.id == goal_id).first()
+    if goal:
+        db.delete(goal)
+        db.commit()
+    db.close()
+    return {"message": "削除しました"}
